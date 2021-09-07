4c990356503062051ffc88f347586ccace6fc4a1
Franco Muria <fmurina@udesa.edu.ar>
2021-09-07 17:19:14 -0300

    algunos cambios

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,60 @@
 import React, { Component } from "react";
+import Tarjeta from "../Tarjeta/Tarjeta"
+
+let key = "3801289076602860794bddb717c8f4f5"
+let api =`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`
 
 export default class Main extends Component{
     constructor(props){
         super(props);
         this.state={
-
+            pelicula:[]
         }
     }
+    componentDidMount(){
+        fetch(api)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                pelicula:data.results,
+                filteredPelicula:data.results,
+            })
+        })
+        .catch(error => console.log(error));
+    }
+    expandirPelicula(){
+
+    }
+    eliminarPelicula(id){
+        let peliculaFiltradas = this.state.pelicula.filter(pelicula => pelicula.id !== id)
+        console.log(peliculaFiltradas); 
+        this.setState({
+            pelicula:peliculaFiltradas,
+            filteredPelicula:peliculaFiltradas,
+    })
+}
+
     render(){
-        return(
+        console.log(this.state.pelicula)
+        //console.log(this.state.pelicula[0])
+    return(
             <main>
+                
+                <h4>Cargando... </h4>
+                {this.state.pelicula.map((pelicula,index)=>{
+                    return <Tarjeta
+                    key={index}
+                    foto={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`}
+                    titulo={pelicula.title}//Funciona
+                    descripcion={pelicula.overview}//Funciona
+                    // vermas={(titulo)=>this.expandirPelicula(titulo)}
+                    idioma={pelicula.original_language}
+                    estreno={pelicula.release_date}
+                    popularidad={pelicula.popularity}
+                    eliminar={(id)=>this.eliminarPelicula(id)}
+                    ></Tarjeta>
+                })}
                 
             </main>
         )
---
+++ src/components/Tarjeta/Tarjeta.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import './style.css'
+export default class Tarjeta extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            clase:'esconder',
+            mensaje:"Ver mas"
+        };
+    }
+    handleShow(){
+        if(this.state.clase === 'esconder'){
+        this.setState({
+        clase:'mostrar',
+        mensaje:"Ver menos"
+        })
+    }else{
+        this.setState({
+            clase:'esconder',
+            mensaje:"Ver mas"
+        })
+    }}
+    render(){
+
+        return(
+            <div>
+            <article>
+                <section class="navigation">
+                    <div>
+                        <i class="fas fa-chevron-left"></i>
+                        <i class="fas fa-chevron-right"></i>
+                    </div>
+                    <i class="far fa-window-close"></i>
+                </section>
+                <main>
+                    <img src={this.props.foto} alt="Cargando"/>
+                    <h3>{this.props.titulo}</h3>
+                    <p className="description">{this.props.descripcion}</p>
+                    <p className='mas' onClick={()=> this.handleShow()}>{this.state.mensaje}</p>
+                    <section className={this.state.clase}>
+                        <ul>
+                            <li>Idioma original: {this.props.idioma}</li>
+                            <li>Fecha de estreno: {this.props.estreno}</li>       
+                            <li>Popularidad: {this.props.popularidad}</li>
+                        </ul>
+                    </section>
+                    {/* <a onClick={()=>this.props.vermas(this.props.name)} href="https://www.google.com/?client=safari">Ver más</a> */}
+                    <button onClick={()=>this.props.eliminar(this.props.id)}>Borrar pelicula</button>
+                </main>
+            </article>
+            </div>
+        )
+    }
+
+};
+
